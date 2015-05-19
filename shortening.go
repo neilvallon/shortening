393b5772eb6ef753411dd6c4e79171218bd9c934
Neil Vallon <neilvallon@gmail.com>
2015-05-19 06:23:02 +0000

    unroll encode loop
    go version go1.4.2 darwin/amd64
    benchmark           old ns/op     new ns/op     delta
    BenchmarkEncode     79.7          76.6          -3.89%
    BenchmarkDecode     34.5          34.6          +0.29%
    
    go version devel +b21ff39 Tue May 19 02:18:40 2015 +0000 darwin/amd64
    benchmark           old ns/op     new ns/op     delta
    BenchmarkEncode     75.2          69.9          -7.05%
    BenchmarkDecode     37.1          37.5          +1.08%

--- shortening.go
+++ shortening.go
@@ -1,28 +1,78 @@
 package shortening // import "vallon.me/shortening"
 
-import (
-	"errors"
-	"math"
-)
+import "errors"
 
 var (
 	charSet     = []byte(`ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_`)
 	lookupTable = makeTable(charSet)
 )
 
+const (
+	// Min values are the smallest value of 'n' that uses the number of
+	// place values.
+	min02 = 1 << 6
+	min03 = (min02 + 1) << 6
+	min04 = (min03 + 1) << 6
+	min05 = (min04 + 1) << 6
+	min06 = (min05 + 1) << 6
+	min07 = (min06 + 1) << 6
+	min08 = (min07 + 1) << 6
+	min09 = (min08 + 1) << 6
+	min10 = (min09 + 1) << 6
+	min11 = (min10 + 1) << 6
+)
+
 // Encode turns an uint64 into a slice of characters from 'charSet'
 func Encode(n uint64) []byte {
 	var buf [11]byte
-	var i int
+	start := 10
 
-	for {
-		buf[i], n = charSet[n&63], (n>>6)-1
-		i++
-
-		if n == math.MaxUint64 {
-			return buf[:i]
-		}
+	switch {
+	case min11 <= n:
+		buf[0], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min10 <= n:
+		buf[1], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min09 <= n:
+		buf[2], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min08 <= n:
+		buf[3], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min07 <= n:
+		buf[4], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min06 <= n:
+		buf[5], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min05 <= n:
+		buf[6], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min04 <= n:
+		buf[7], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min03 <= n:
+		buf[8], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	case min02 <= n:
+		buf[9], n = charSet[n&63], (n>>6)-1
+		start--
+		fallthrough
+	default:
+		buf[10] = charSet[n&63]
 	}
+
+	return buf[start:]
 }
 
 // Decode turns a slice of characters back into the original unit64.
@@ -96,7 +146,7 @@ func Decode(b []byte) (n uint64, err error) {
 
 	// 1171221845949812800 is the minimum value to have len == 11
 	// any lower value is an overflow.
-	if len(b) == 11 && n-1 < 1171221845949812800 {
+	if len(b) == 11 && n-1 < min11 {
 		return 0, errors.New("shortening: int64 overflow")
 	}
 
