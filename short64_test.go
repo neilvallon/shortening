9ed42c0dc0fe8eb65a877728b8eed82451eb40de
Neil Vallon <neilvallon@gmail.com>
2017-09-22 11:50:31 -0500

    factor out test table

--- short64_test.go
+++ short64_test.go
@@ -7,33 +7,32 @@ import (
 	"time"
 )
 
+var b64tests = []struct {
+	ID  uint64
+	URL string
+}{
+	{0, "A"},
+	{1, "B"},
+	{2, "C"},
+	{min02 - 1, "_"},
+	{min02, "AA"},
+	{min02 + 1, "BA"},
+	{min02 + 2, "CA"},
+	{min02 * 2, "AB"},
+	{min03 - 1, "__"},
+	{min03, "AAA"},
+	{min06, "AAAAAA"},
+
+	{min11 - 1, "__________"},
+	{min11, "AAAAAAAAAAA"},
+	{min11 + 1, "BAAAAAAAAAA"},
+
+	{math.MaxUint64 - 1, "----------O"},
+	{math.MaxUint64, "_---------O"},
+}
+
 func TestEncodeShareInt(t *testing.T) {
-	tests := []struct {
-		ID  uint64
-		URL string
-	}{
-		{0, "A"},
-		{1, "B"},
-		{2, "C"},
-		{63, "_"},
-		{64, "AA"},
-		{65, "BA"},
-		{66, "CA"},
-		{128, "AB"},
-		{128, "AB"},
-		{4159, "__"},
-		{4160, "AAA"},
-		{1090785344, "AAAAAA"},
-
-		{1171221845949812800 - 1, "__________"},
-		{1171221845949812800, "AAAAAAAAAAA"},
-		{1171221845949812800 + 1, "BAAAAAAAAAA"},
-
-		{math.MaxUint64 - 1, "----------O"},
-		{math.MaxUint64, "_---------O"},
-	}
-
-	for _, test := range tests {
+	for _, test := range b64tests {
 		if e := string(Encode(test.ID)); e != test.URL {
 			t.Errorf("got: %q - expected: %q", e, test.URL)
 		}
@@ -41,32 +40,7 @@ func TestEncodeShareInt(t *testing.T) {
 }
 
 func TestDecodeShareInt(t *testing.T) {
-	tests := []struct {
-		ID  uint64
-		URL string
-	}{
-		{0, "A"},
-		{1, "B"},
-		{2, "C"},
-		{63, "_"},
-		{64, "AA"},
-		{65, "BA"},
-		{66, "CA"},
-		{128, "AB"},
-		{128, "AB"},
-		{4159, "__"},
-		{4160, "AAA"},
-		{1090785344, "AAAAAA"},
-
-		{1171221845949812800 - 1, "__________"},
-		{1171221845949812800, "AAAAAAAAAAA"},
-		{1171221845949812800 + 1, "BAAAAAAAAAA"},
-
-		{math.MaxUint64 - 1, "----------O"},
-		{math.MaxUint64, "_---------O"},
-	}
-
-	for _, test := range tests {
+	for _, test := range b64tests {
 		n, err := Decode([]byte(test.URL))
 		if err != nil {
 			t.Logf("%q", test.URL)
@@ -124,7 +98,7 @@ func TestEncDecParity(t *testing.T) {
 	// test random 10k
 	var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < 100000; i++ {
-		v := uint64(rnd.Uint32())
+		v := rnd.Uint64()
 
 		n, err := Decode(Encode(v))
 		if err != nil {
@@ -142,7 +116,7 @@ const benchSet = 10000
 func BenchmarkEncode(b *testing.B) {
 	ids := make([]uint64, benchSet)
 	for i := range ids {
-		ids[i] = uint64(rand.Int63())
+		ids[i] = rand.Uint64()
 	}
 
 	b.ResetTimer()
@@ -154,7 +128,7 @@ func BenchmarkEncode(b *testing.B) {
 func BenchmarkDecode(b *testing.B) {
 	urls := make([][]byte, benchSet)
 	for i := range urls {
-		urls[i] = Encode(uint64(rand.Int63()))
+		urls[i] = Encode(rand.Uint64())
 	}
 
 	b.ResetTimer()
